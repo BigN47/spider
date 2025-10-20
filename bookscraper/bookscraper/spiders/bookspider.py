bcf75c3e1a82364f07a78fa0b13822946f81ae18
Big-N <hongsonics@gmail.com>
2025-10-20 12:23:56 +0100

    Add files via upload

---
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -0,0 +1,21 @@
+
+
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        books = response.css("article.product_pod")
+        for book in books:
+            yield{
+                   "link": book.css("h3 a::attr(href)").get(),
+                   "price": book.css("div.product_price p.price_color::text").get(),
+                   "title": book.css("h3 a::attr(title)").get()}
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+             next_page_url = "https://books.toscrape.com" + next_page
+             yield response.follow(next_page, callback=self.parse)
